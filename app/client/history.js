5140a7712e49261d2d077bf99ec21976e58a2a78
Eunsub Kim <eunsub@gmail.com>
2014-06-12 11:35:30 +0900

    recursively detect relation path

--- app/client/history.js
+++ app/client/history.js
@@ -8,21 +8,29 @@ Template.history.helpers({
 
 
     console.log("WORK_ITEM:", workItem);
+    var solution = Cento.Solutions.findOne({_id: workItem.solution_id});
 
-    if(workItem.type == 'modeling'){
-      paths.push(workItem);
+    var relatedItems = function(item){
+      paths.push(item);
+      var relatedId = null;
+      try {
+        relatedId = _.detect(item.related, function(i){
+          return i.type == 'reference';
+        }).related_work_id;
+      }catch(e){}
+      if(relatedId){
+        var i = Cento.WorkItems.findOne({_id: relatedId});
+        if(i) relatedItems(i);
+      }
+    }
+
+    relatedItems(workItem);
 
 
-      var projectIdeation = Cento.WorkItems.findOne({_id: workItem.related[0].related_work_id});
-      paths.push(projectIdeation);
-
-      var solution = Cento.Solutions.findOne({_id: workItem.solution_id});
-      paths.push(solution);
-
-
+    paths.push(solution);
+    if(solution.related && solution.related.length > 0){
       var userNeed = Cento.WorkItems.findOne({_id: solution.related[0].related_work_id});
       paths.push(userNeed);
-
     }
 
 
